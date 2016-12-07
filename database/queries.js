6cbfe4f7e131dbcc097a8f8090490c540ac0af60
Rachel Ralston <rachelralston@gmail.com>
2016-12-07 08:40:21 -0800

    got first appointment route written and test partially written

--- database/queries.js
+++ database/queries.js
@@ -9,10 +9,45 @@ const createRecord = (table, attributes) =>
     .returning('*')
     .then(firstRecord)
 
+const findRecordByLgId = (table, attributes) =>
+  knex
+    .table(table)
+    .where('lg_id', attributes)
+    .first('*')
+
+const updateRecordbyLgId = (table, lg_id, attributes) =>
+  knex
+    .table(table)
+    .where('lg_id', lg_id)
+    .update(attributes)
+    .returning('*')
+    .then(firstRecord)
+
+const deleteRecordbyLgId = (table, lg_id) =>
+  knex
+    .table(table)
+    .where('lg_id', lg_id)
+    .del()
+
 const createUser = attributes =>
   createRecord('users', attributes).then( user => user )
 
+const findUser = lg_id => 
+  findRecordByLgId('users', lg_id).then(user => user)
+
+const updateUser = (lg_id, attributes) => 
+  updateRecordbyLgId('users', lg_id, attributes).then(user => user)
+
+const deleteUser = lg_id =>
+  deleteRecordbyLgId('users', lg_id)
+
+const createAppointment = attributes =>
+  createRecord('appointments', attributes).then( appointment => appointment )
+
 module.exports = {
   knex,
-  createUser
+  createUser, 
+  findUser,
+  updateUser,
+  deleteUser
 }
--- test/queries/queriesTest.js
+++ test/queries/queriesTest.js
@@ -1,17 +1,17 @@
-const {createUser} = require('../../database/queries')
+const {createUser, findUser, updateUser, deleteUser} = require('../../database/queries')
 const {expect, app, chai} = require('../setup')
 
 describe('QUERIES', () => {
+  const user = {
+    lg_id: '1234ab',
+    can_coach: true,
+    active_calender: true,
+    active_coach: false,
+    google_token: "A_TOKEN"
+  }
+
   describe('Inserts new user', () => {
     it('should insert a user into the database', done => {
-      const user = {
-        lg_id: '1234ab',
-        can_coach: true,
-        active_calender: true,
-        active_coach: false,
-        google_token: "A_TOKEN"
-      }
-
       createUser(user)
         .then(user => {
           console.log(user)
@@ -25,4 +25,78 @@ describe('QUERIES', () => {
         })
     })
   })
+
+  describe('findUser', () => {
+    it('should find a user', done => {
+      createUser(user)
+        .then(user => {
+          findUser(user.lg_id)
+            .then(user => {
+              console.log(user)
+              expect(user).to.be.a('object')
+              expect(user.lg_id).to.eql('1234ab')
+              expect(user.can_coach).to.eql(true)
+              expect(user.active_calender).to.eql(true)
+              expect(user.active_coach).to.eql(false)
+              expect(user.google_token).to.eql("A_TOKEN")
+              done()
+            })
+        })
+    })
+  })
+
+  describe('updateUser', () => {
+    it('should update a user record', done => {
+      createUser(user)
+        .then(user => {
+          updateUser(user.lg_id, { active_coach: true })
+            .then(user => {
+              console.log(user)
+              expect(user).to.be.a('object')
+              expect(user.lg_id).to.eql('1234ab')
+              expect(user.can_coach).to.eql(true)
+              expect(user.active_calender).to.eql(true)
+              expect(user.active_coach).to.eql(true)
+              expect(user.google_token).to.eql("A_TOKEN")
+              done()
+            })
+        })
+    })
+  })
+
+  describe('deleteUser', () => {
+    it('should delete a user record', done => {
+      createUser(user)
+        .then(user => {
+          deleteUser(user.lg_id)
+            .then(user => {
+              //knex sets no user in db equal to 1
+              expect(user).to.eql(1)
+              done()
+            })
+        })
+    })
+  })
+
+  // const appointment = {
+  //   coach_id:'1234ab',
+  //   date_time: 1900-01-01 00:00:00,
+    
+  // }
+
+  // describe('Inserts new user', () => {
+  //   it('should insert a user into the database', done => {
+  //     createUser(user)
+  //       .then(user => {
+  //         console.log(user)
+  //         expect(user).to.be.a('object')
+  //         expect(user.lg_id).to.eql('1234ab')
+  //         expect(user.can_coach).to.eql(true)
+  //         expect(user.active_calender).to.eql(true)
+  //         expect(user.active_coach).to.eql(false)
+  //         expect(user.google_token).to.eql("A_TOKEN")
+  //         done()
+  //       })
+  //   })
+  // })
 })
