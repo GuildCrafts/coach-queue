2847c482f81de8e9dbbef9335f4f73104be50645
Rachel Ralston <rachelralston@gmail.com>
2016-12-13 17:58:44 -0800

    WIP organizing free times

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const gcal = require('google-calendar')
+const moment = require('moment')
 const {getActiveCoaches} = require('../io/database/users')
 
 router.all('/', (req, res) => {
-  var accessToken = req.session.access_token
+  const accessToken = req.session.access_token
 
   gcal(accessToken).calendarList.list((err, data) => 
     err ? res.send(500,err) : res.json(data)
   )
 })
-
 
 router.all('/:calendarId', (req, res) => {
   const {access_token} = req.session;
@@ -20,27 +20,37 @@ router.all('/:calendarId', (req, res) => {
   { 
     items: [{id:`${calendarId}`}],
     timeMin: new Date(), 
-    timeMax: new Date("2016-12-20")
+    timeMax: new Date("2016-12-20"),
+    timezone: 'America/Los_Angeles'
   }, (err, data) => {
-    // if (err) return res.send(500,err)
+    if (err) { return res.send(500,err) }
 
-    const busyTimeMap = data.calendars[calendarId].busy
+    let busyTimeMap = data.calendars[calendarId].busy
+    // console.log('busyTimeMap = ', busyTimeMap)
+    console.log('your current moment is ', moment().seconds(30))
 
     let dayStartTime = '9am'
     let dayEndTime = '5:30pm'
+    let currentTime = Date()
 
-
-
-    // [ { end: '2016-12-15T18:00:00Z', start: '2016-12-15T19:00:00Z' } ]
-
-    // [{start:dayStartTime, end:busyTimeMap[0].end},
-    //   {start:busyTimeMap[0].start, end:dayEndTime}]
-
-
-    let thing = busyTimeMap.reduce((freetimes, current) => {
-      console.log('current!', current)
-      freetimes.push()
+    let thing = busyTimeMap.reduce((freetimes, currentAppt) => {
+      console.log('current!', currentAppt)
+      console.log('--current apt.start: ', currentAppt.start, '--currentTime: ', currentTime)
+      if(currentAppt.start >= currentTime) {
+        freetimes.push({start:currentTime, end: currentAppt.start})
+        currentTime = currentAppt.end
+        console.log('freetimes ',freetimes)
+        return freetimes
+      } else if(!currentAppt){
+        console.log('in not currentAppt')
+        return freetimesfreetimes.push({start:currentTime, end: '6pm'})
+      } else {
+        console.log('final else')
+        return current = currentAppt.end
+      }
     }, [])
+
+    console.log('FINAL THING =====>', thing)
 
     return res.json(data);
   });
