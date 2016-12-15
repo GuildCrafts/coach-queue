e9648a9d9c1ac13d238057149479b54d636ad283
Rachel Ralston <rachelralston@gmail.com>
2016-12-15 10:13:38 -0800

    updated testing dummy data

--- models/appointment.js
+++ models/appointment.js
@@ -9,39 +9,43 @@ const findFreeSchedule = (busyTime) => {
     ? moment("2016-12-14T09:00:00.000") 
     : moment()
 
-  // console.log(moment(currentTime, "YYYY-MM-DD HH:mm"))
-  // console.log(moment().utcOffset("-08:00"))
+
+  // { start: '2016-12-14T10:30:00Z', end: '2016-12-14T11:30:00Z' },
+  // { start: '2016-12-14T15:00:00Z', end: '2016-12-14T16:00:00Z' },
+
   
   let freeApptTimes = busyTime.reduce((freetimes, currentAppt) => {
     let startTime = moment(currentAppt.start).utcOffset("-08:00")
     let endTime = moment(currentAppt.end).utcOffset("-08:00")
     console.log('START', startTime)
     console.log('END', endTime)
-    console.log('moment NOW', moment())
+    console.log('currentAppt', currentAppt)
 
-
-    
-    if ((startTime && endTime).isBetween(dayEndTime, dayStartTime)) {
-      return freetimes
-    } 
-
-    else if (startTime >= currentTime) {
-      if (currentTime.isBetween(dayStartTime, dayEndTime)) {
-        freetimes.push({start:currentTime, end: startTime})
-        currentTime = endTime
-        return freetimes
-      }
-      currentTime = endTime
-      return freetimes 
-    } 
-
-    else {
-      current = endTime
-      return freetimes
+    if(startTime >= currentTime) {
+      console.log('start time is greater than current time')
     }
+    
+    // if ((startTime && endTime).isBetween(dayEndTime, dayStartTime)) {
+    //   return freetimes
+    // } 
+
+    // if (startTime >= currentTime) {
+    //   if (currentTime.isBetween(dayStartTime, dayEndTime)) {
+    //     freetimes.push({start:currentTime, end: startTime})
+    //     currentTime = endTime
+    //     return freetimes
+    //   }
+    //   currentTime = endTime
+    //   return freetimes 
+    // } 
+
+    // else {
+    //   current = endTime
+    //   return freetimes
+    // }
   }, [])
  
-  freeApptTimes.push({start:currentTime, end:dayEndTime})
+  // freeApptTimes.push({start:currentTime, end:dayEndTime})
   return freeApptTimes
 }
 
--- test/models/appointment.js
+++ test/models/appointment.js
@@ -2,9 +2,6 @@ const {expect, app, chai} = require('../setup')
 const {findFreeSchedule} = require('../../models/appointment')
 const {busyTimeData} = require('./appointmentTestData')
 
-// { start: '2016-12-14T10:30:00Z', end: '2016-12-14T11:30:00Z' },
-//   { start: '2016-12-14T15:00:00Z', end: '2016-12-14T16:00:00Z' },
-
 describe('Appointment Models: ', () => {
   describe('Change Gcal busy time to available time.', () => {
     it('should give us free time', done => {
