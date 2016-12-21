8f96ccd3a20ad01adb409c8431c70b84ba7e1691
Rachel Ralston <rachelralston@gmail.com>
2016-12-21 08:41:26 -0800

    syntax

--- routes/google.js
+++ routes/google.js
@@ -11,10 +11,12 @@ router.get('/auth/callback',
   (request, response) => {
     request.session.access_token = request.user.accessToken;
     const {access_token, github_handle} = request.session
- // TODO access token not being updated when you relogin into google
-    github_handle
-      ? updateUserByHandle(github_handle, {google_token: access_token})
-      : response.redirect('/');
+    // TODO access token not being updated when you relogin into google
+    if(github_handle) {
+      updateUserByHandle(github_handle, {google_token: access_token})
+    } else {
+      response.redirect('/');
+    }
   });
 
 module.exports = router;
