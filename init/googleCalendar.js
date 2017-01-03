1d1994c7b637e20286f556dc1b09b4593088e62e
Punit Rathore <punitr@gmail.com>
2017-01-03 13:38:30 -0800

    updating production config

--- init/googleCalendar.js
+++ init/googleCalendar.js
@@ -5,8 +5,8 @@ const init = function(expressApp, config) {
   expressApp.use(passport.initialize())
 
   passport.use(new GoogleStrategy({
-      clientID: config.google.clientID,
-      clientSecret: config.google.client_secret,
+      clientID: process.env.GOOGLE_CLIENTID || config.google.clientID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || config.google.client_secret,
       callbackURL: config.host_fully_qualified + config.google.callbackURLPath,
       scope: config.google.scopes
     },
