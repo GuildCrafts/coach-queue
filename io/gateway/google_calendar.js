bce21197160c52c6d30f9931cc1694fdea7dbafd
Punit Rathore <punitr@gmail.com>
2017-01-04 16:29:46 -0800

    refreshing the access token everytime a request for a coach is made

---
+++ io/gateway/google_calendar.js
@@ -0,0 +1,23 @@
+const google = require('googleapis')
+const plus = google.plus('v1')
+const OAuth2 = google.auth.OAuth2
+const config = require('../../config/config').readConfig()
+const oauth2Client = new OAuth2(
+  process.env.GOOGLE_CLIENTID || config.google.clientID,
+  process.env.GOOGLE_CLIENT_SECRET || config.google.client_secret,
+  config.google.callbackURLPath
+);
+const { updateUserByHandle } = require('../database/users')
+
+const refreshAccessTokenAsync = (access_token, refresh_token, github_handle) => {
+  oauth2Client.setCredentials({
+    refresh_token: refresh_token
+  });
+
+  oauth2Client.refreshAccessToken(function(err, tokens) {
+    updateUserByHandle(github_handle,
+                       {google_token: tokens.access_token});
+  });
+};
+
+module.exports = {refreshAccessTokenAsync};
