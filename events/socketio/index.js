4d9d438edcd00a4f67852c00b18aa37a9dc27125
jrob8577 <roberts.john@gmail.com>
2017-03-22 14:35:05 -0700

    Added event sync (sink?)
    * env updates to fix redirect bug
    * test to send arbitrary events
    * shells for create, assign, cancel, start, resolve

--- events/socketio/index.js
+++ events/socketio/index.js
@@ -1,3 +1,19 @@
+const events = require( '../' )
 const io = require( 'socket.io' )()
+
+io.on( 'connection', socket => {
+  socket.on( 'join', room => socket.join( room ))
+
+  socket.on( 'client-test', data =>
+    io.to( '/tests' ).emit( 'test-pingback', data )
+  )
+
+  for( let event in events ) {
+    socket.on( event, events[ event ])
+    socket.on( event, data => {
+      io.to( '/tests' ).emit( 'test-pingback', { message: `Received event ${event}` } )
+    })
+  }
+})
 
 module.exports = io
