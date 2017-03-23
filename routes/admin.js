609350a7b2f435adc466cea2682546334b06ec4d
Amelia Clarke <ameliavoncat@gmail.com>
2017-03-23 11:29:29 -0700

    Admin UI in progress

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,16 @@ var router = express.Router()
 
 /* GET admin landing page. */
 router.get('/', function( request, response, next ) {
-  response.render('admin', { title: 'Admin' })
+  response.render('admin-page', {
+    title: 'Admin',
+    coaches: ['Amelia', 'Diana'],
+    teams: ['fanciful-moose', 'oblong-penguin'],
+    active_coach_data: [{
+      coach: 'Amelia',
+      learners: ['Kim', 'Bob'],
+      projects: ['oblong-penguin']
+    }]
+  })
 })
 
 module.exports = router
