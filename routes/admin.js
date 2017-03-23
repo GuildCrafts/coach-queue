4c87db91de9c0d99b14b564924f7d1577392fc4d
Amelia Clarke <ameliavoncat@gmail.com>
2017-03-23 16:28:23 -0700

    completed mvp admin page

--- routes/admin.js
+++ routes/admin.js
@@ -5,13 +5,25 @@ var router = express.Router()
 router.get('/', function( request, response, next ) {
   response.render('admin-page', {
     title: 'Admin',
-    coaches: ['Amelia', 'Diana'],
-    teams: ['fanciful-moose', 'oblong-penguin'],
-    active_coach_data: [{
-      coach: 'Amelia',
-      learners: ['Kim', 'Bob'],
-      projects: ['oblong-penguin']
-    }]
+    page_data: {
+      showing_modal: false
+    }
+  })
+})
+
+router.get('/assign-coaches', function( request, response, next ) {
+  response.render('admin-page', {
+    title: 'Admin',
+    page_data: {
+      coaches: ['Amelia', 'Diana'],
+      projects: ['core-team-naming', 'advanced-team-naming'],
+      active_coach_data: [{
+        name: 'Amelia',
+        assigned_learners: ['Kim', 'Bob'],
+        assigned_projects: ['core-team-naming']
+      }],
+      showing_modal: true
+    }
   })
 })
 
