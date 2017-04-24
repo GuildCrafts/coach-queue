ea93a30e79c4e84de5a7076b8d6c1837836e430c
jrob8577 <roberts.john@gmail.com>
2017-04-24 10:42:00 -0700

    fml

--- middleware/setLocals.js
+++ middleware/setLocals.js
@@ -2,17 +2,17 @@ const db = require( '../database/' )
 const { Statistics } = db
 
 const setLocals = (request, response, next) => {
-  Statistics.forCoach( request.user.handle )
-    .then( statistics => {
+  // Statistics.forCoach( request.user.handle )
+  //   .then( statistics => {
       response.locals = {
         user: request.user,
         development: process.env.NODE_ENV === 'development',
-        statistics
+        statistics: {}
       }
 
       next()
 
-    })
+    // })
 }
 
 module.exports = setLocals
