b62a5528f56cfaec097893c38086d4d3fd813f7a
jrob8577 <roberts.john@gmail.com>
2017-04-24 11:20:30 -0700

    This *should* work now

--- middleware/setLocals.js
+++ middleware/setLocals.js
@@ -2,8 +2,8 @@ const db = require( '../database/' )
 const { Statistics } = db
 
 const setLocals = (request, response, next) => {
-  // Statistics.forCoach( request.user.handle )
-  //   .then( statistics => {
+  Statistics.forCoach( request.user.handle )
+    .then( statistics => {
       response.locals = {
         user: request.user,
         development: process.env.NODE_ENV === 'development',
@@ -12,7 +12,7 @@ const setLocals = (request, response, next) => {
 
       next()
 
-    // })
+    })
 }
 
 module.exports = setLocals
