f930ffe35d9040a239025d2d4a267831ce626802
John Roberts <roberts.john@gmail.com>
2017-04-24 17:58:29 -0700

    [Fixes #262] Queue overview updates (#278)
    * Added popover that contains claims, escalations, primary coach, age, goal, claim status

--- public/javascripts/admin/summary.js
+++ public/javascripts/admin/summary.js
@@ -23,8 +23,31 @@ const clearEvents = ({ coaches }) => () => {
 const pastThreshold = created_at =>
   moment.duration( moment().diff( moment( created_at )) ).asMinutes() >= THRESHOLD
 
-const eventReceived = clear => ({ requests }) => {
+let localRequests = []
+
+const requestButton = ( request, adminData ) => {
+  const label = pastThreshold( request.created_at ) ? 'danger' : 'success'
+  const currentEvent = request.events[ request.events.length - 1 ]
+
+  const claimedBy = currentEvent.name === 'claim' ?
+    currentEvent.data.by : 'Unclaimed'
+
+  return `
+    <a class="btn btn-sm btn-${label} team-button"
+      data-coach="${request.goal.coach_id}" data-created-at="${request.created_at}"
+      data-toggle="popover" data-trigger="focus"
+      data-claimed-by="${claimedBy}"
+      data-escalations="${request.events.filter( e => e.name === 'escalate').length}"
+      data-claims="${request.events.filter( e => e.name === 'claim').length}"
+      data-name="${request.team[ 0 ].name}" data-created-at="${request.events[ 0 ].created_at}"
+      data-id="${request.id}"
+      title="${request.team[ 0 ].name}">${request.team[ 0 ].name}</a>
+  `
+}
+
+const eventReceived = ( clear, adminData ) => ({ requests }) => {
   clear()
+  localRequests = requests
 
   requests.forEach( request => {
     const assignedCoach = request.goal.coach_id
@@ -32,31 +55,78 @@ const eventReceived = clear => ({ requests }) => {
     if( pastThreshold( request.created_at )) {
       const el = document.querySelector( `tr.coach-${assignedCoach} td:nth-child(2)` )
 
-      el.innerHTML = el.innerHTML + `\n<label class="label label-danger">${request.team[0].name}</label>`
+      el.innerHTML = el.innerHTML + requestButton( request, adminData ) // + `\n<label class="label label-danger">${request.team[0].name}</label>`
     } else {
       const el = document.querySelector( `tr.coach-${assignedCoach} td:nth-child(3)` )
 
-      el.innerHTML = el.innerHTML + `\n<label class="label label-success" data-coach="${assignedCoach}" data-created-at="${request.created_at}">${request.team[0].name}</label>`
+      el.innerHTML = el.innerHTML + requestButton( request, adminData ) //`\n<label class="label label-success" data-coach="${assignedCoach}" data-created-at="${request.created_at}">${request.team[0].name}</label>`
     }
   })
 }
 
-const ageRequests = () => {
-  const requests = Array.from( document.querySelectorAll( '.label-success' ) )
+const ageRequests = adminData => () => {
+  const requests = Array.from( document.querySelectorAll( '.btn-success' ) )
 
   requests.forEach( request => {
     if( pastThreshold( request.dataset.createdAt )) {
+      const requestId = parseInt( request.dataset.id )
       request.parentNode.removeChild( request )
 
       const el = document.querySelector( `tr.coach-${request.dataset.coach} td:nth-child(2)` )
 
-      el.innerHTML = el.innerHTML + `\n<label class="label label-danger">${request.innerText}</label>`
+      const oldRequest = localRequests.find( r => r.id === requestId )
+      el.innerHTML = el.innerHTML + requestButton( oldRequest, adminData )
     }
   })
 }
 
+const popoverContent = ({ teams, goal_counts, coaches }) => function() {
+  const el = $(this)
+
+  const team = teams.find( t => t.name === el.data('name'))
+  const { goal_id } = team
+
+  const goal = goal_counts.find( g => g.goal_id === goal_id )
+  const { coach_id } = goal
+
+  const coach = coaches.find( c => c.id === coach_id )
+
+  return `
+    <table class="table table-condensed">
+      <tr>
+        <th>Goal</th>
+        <td><a href="${goal.link}" target="_blank">${goal.title}</a></td>
+      </tr>
+      <tr>
+        <th>Claimed By</th>
+        <td>${el.data('claimed-by')}</td>
+      </tr>
+      <tr>
+        <th>Claims</th>
+        <td>${el.data('claims')}</td>
+      </tr>
+      <tr>
+        <th>Escalations</th>
+        <td>${el.data('escalations')}</td>
+      </tr>
+      <tr>
+        <th>Primary Coach</th>
+        <td>${coach.handle}</td>
+      </tr>
+      <tr>
+        <th>Created At</th>
+        <td>
+          ${moment( el.data('created-at') ).format('M/D H:mm')}<br />
+          (${moment.duration( moment().diff( moment( el.data('created-at') )) ).humanize()} old)
+        </td>
+      </tr>
+    </table>
+  `
+}
+
 const setup = ([ adminData, requests ]) => {
-  const render = eventReceived( clearEvents( adminData ))
+  localRequests = requests
+  const render = eventReceived( clearEvents( adminData ), adminData )
 
   addCoachRows( adminData )
   render({ requests })
@@ -64,7 +134,15 @@ const setup = ([ adminData, requests ]) => {
   socket.emit( 'join', '/events' )
   socket.on( 'event', render )
 
-  setInterval( ageRequests, 60000 )
+  setInterval( ageRequests( adminData ), 60000 )
+
+  $('body').popover({
+    placement: 'bottom',
+    container: 'body',
+    html: true,
+    selector: '[data-toggle="popover"]',
+    content: popoverContent( adminData )
+  })
 }
 
 const PARAMS = {
