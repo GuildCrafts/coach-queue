a078e00bdcfa84222b87e4bb68952a7b53573fe0
John Roberts <roberts.john@gmail.com>
2017-05-08 23:03:40 -0700

    [Fixes #297] Additional query update (#301)
    * More query updates. I shouldn't program on vacation...

--- database/goals/index.js
+++ database/goals/index.js
@@ -1,11 +1,11 @@
 const db = require( '../db' )
 
 const FOR_TEAM = `
-  SELECT goals.id, goals.link, goals.title, teams.id as team_id, players.handle as coach, players.id as coach_id FROM goals
-  JOIN teams ON teams.goal_id=goals.id
-  JOIN goal_coaches ON (goal_coaches.goal_id=goals.id AND goal_coaches.cycle=teams.cycle)
-  JOIN players ON goal_coaches.coach_id=players.id
-  WHERE teams.id=$1
+  select goals.id, goals.link, goals.title, teams.id as team_id, coach_id, players.handle from teams
+  join goal_coaches on goal_coaches.goal_id=teams.goal_id and goal_coaches.cycle=teams.cycle
+  join goals on goal_coaches.goal_id=goals.id
+  join players on players.id=coach_id
+  where teams.id=$1
 `
 
 module.exports = {
